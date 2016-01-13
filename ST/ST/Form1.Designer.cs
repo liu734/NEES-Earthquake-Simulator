9e2fa3a08d0d8cd1cbf9c559978ae21cc34288c1
liu734 <liujingye0830@gmail.com>
2016-01-13 14:15:08 -0500

    update

---
+++ ST/ST/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace ST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SpPerfChart.ChartPen chartPen1 = new SpPerfChart.ChartPen();
+            SpPerfChart.ChartPen chartPen2 = new SpPerfChart.ChartPen();
+            SpPerfChart.ChartPen chartPen3 = new SpPerfChart.ChartPen();
+            SpPerfChart.ChartPen chartPen4 = new SpPerfChart.ChartPen();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.perfChart1 = new SpPerfChart.PerfChart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(25, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 33);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Go";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(160, 251);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Frequency";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Amplitude";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 14;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(528, 475);
+            this.tabControl1.TabIndex = 15;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(520, 446);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Frequency";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.perfChart1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label10);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(520, 446);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sensor";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // perfChart1
+            // 
+            this.perfChart1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.perfChart1.Location = new System.Drawing.Point(172, 55);
+            this.perfChart1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.perfChart1.Name = "perfChart1";
+            this.perfChart1.PerfChartStyle.AntiAliasing = true;
+            chartPen1.Color = System.Drawing.Color.Black;
+            chartPen1.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen1.Width = 1F;
+            this.perfChart1.PerfChartStyle.AvgLinePen = chartPen1;
+            this.perfChart1.PerfChartStyle.BackgroundColorBottom = System.Drawing.Color.DarkGreen;
+            this.perfChart1.PerfChartStyle.BackgroundColorTop = System.Drawing.Color.DarkGreen;
+            chartPen2.Color = System.Drawing.Color.Black;
+            chartPen2.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen2.Width = 1F;
+            this.perfChart1.PerfChartStyle.ChartLinePen = chartPen2;
+            chartPen3.Color = System.Drawing.Color.Black;
+            chartPen3.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen3.Width = 1F;
+            this.perfChart1.PerfChartStyle.HorizontalGridPen = chartPen3;
+            this.perfChart1.PerfChartStyle.ShowAverageLine = true;
+            this.perfChart1.PerfChartStyle.ShowHorizontalGridLines = true;
+            this.perfChart1.PerfChartStyle.ShowVerticalGridLines = true;
+            chartPen4.Color = System.Drawing.Color.Black;
+            chartPen4.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen4.Width = 1F;
+            this.perfChart1.PerfChartStyle.VerticalGridPen = chartPen4;
+            this.perfChart1.ScaleMode = SpPerfChart.ScaleMode.Absolute;
+            this.perfChart1.Size = new System.Drawing.Size(313, 134);
+            this.perfChart1.TabIndex = 8;
+            this.perfChart1.TimerInterval = 100;
+            this.perfChart1.TimerMode = SpPerfChart.TimerMode.Disabled;
+            this.perfChart1.Load += new System.EventHandler(this.perfChart1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(87, 163);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(54, 17);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "label10";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(87, 112);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "label9";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(87, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "label8";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 163);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Z";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Y";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "X";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 473);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private SpPerfChart.PerfChart perfChart1;
+    }
+}
+
---
+++ ST/ST/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ST
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        
+        string returnMessage = "";
+        int mod = 0;
+        int amp = 0;  // amplitude
+        int freq = 0;  //frequency
+        int run = 0;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)  //run
+        {
+            // try to open the port
+            try
+            {
+             
+                port = new SerialPort("COM" + textBox3.Text, 9600);
+                port.Open();
+
+                run = 1;
+                mod = 0;
+
+
+
+                try
+                {
+                    freq = Int32.Parse(textBox1.Text);
+                }
+                catch (FormatException ex)
+                {
+                }
+
+
+
+                try
+                {
+                    amp = Int32.Parse(textBox2.Text);
+                }
+                catch (FormatException ex)
+                {
+                }
+
+                send();
+
+ 
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+  
+            port.Close();
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+ 
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DoUpdate(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void serialPort1_DataReceived
+            (object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+    
+            this.Invoke(new EventHandler(DoUpdate));
+        }
+
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)  //stop button
+        {
+            // try to open the port
+            try
+            {
+
+                port = new SerialPort("COM" + textBox3.Text, 9600);
+                port.Open();
+
+
+                run = 0;
+                mod = 0;
+
+
+                try
+                {
+                    freq = Int32.Parse(textBox1.Text);
+                }
+                catch (FormatException ex)
+                {
+                }
+
+
+
+                try
+                {
+                    amp = Int32.Parse(textBox2.Text);
+                }
+                catch (FormatException ex)
+                {
+                }
+
+                send();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+            port.Close();
+        }
+
+
+
+        private void send() { //send signal to 
+            byte[] buffer = new byte[4];
+            buffer[0] = Convert.ToByte(run);  //run or not
+            buffer[1] = Convert.ToByte(mod);  //module one : frequency module
+            buffer[2] = Convert.ToByte(freq);  //send freqency
+            buffer[3] = Convert.ToByte(amp);   //send amplitude
+
+
+
+
+
+            int intReturnASCII = 0;
+            char charReturnValue = (Char)intReturnASCII;
+
+
+            port.Write(buffer, 0, 4);
+
+
+            Thread.Sleep(1000);
+
+
+
+
+            port.Close();
+
+       
+            Application.DoEvents(); //flush the data in the buffer
+        
+        
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                port = new SerialPort("COM" + textBox3.Text, 9600);
+                port.Open();
+
+
+                run = 1;  //make it run
+                mod = 1;  //module 2
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            // number of bytes to read
+            int count = port.BytesToRead;
+           
+
+            int [] inBuf = new int[2]; //initialize a 2 byte array.
+
+            int temp = 0;
+
+            while (run==1) 
+            {
+                
+
+       
+                {
+                    inBuf[0] =  port.ReadByte(); //get 2 bytes first from micor controller
+                    inBuf[1] =  port.ReadByte();
+
+                    if ((inBuf[0] + (inBuf[1] * 256)) == 0xDEAD)
+                    { //check if equal the start tag 0xDEAD
+
+   
+                        for (int i = 0; i < 3;i++ )  // get x y c acceleroment, each size of them is 2 byte
+                        {
+
+                             inBuf[0] = port.ReadByte();
+                            inBuf[1] = port.ReadByte();
+                            temp = 0;
+
+                            if (i == 0)  //x
+                            {
+                                label8.Text = Convert.ToString(inBuf[0] + (inBuf[1] * 256));  // combine 2 byte to 1
+                                temp = temp + (inBuf[0] + (inBuf[1] * 256));
+                            }
+                            else if (i == 1) //y
+                            {
+                                label9.Text = Convert.ToString(inBuf[0] + (inBuf[1] * 256));
+                                temp = temp + (inBuf[0] + (inBuf[1] * 256));
+                            }
+
+                            else if (i == 2) //z
+                            {
+                                label10.Text = Convert.ToString(inBuf[0] + (inBuf[1] * 256));
+                                temp = temp + (inBuf[0] + (inBuf[1] * 256));
+                            }
+
+
+
+                            perfChart1.AddValue(temp/30);  //reduce the size of the acceleroment and put it on the graph. Using the perf Chart.
+
+                           
+
+                            Application.DoEvents();
+                        }
+                        
+
+
+
+
+
+
+                    }
+                    else continue;
+
+                }
+                
+
+            }
+            
+            
+            
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            run = 0;
+            try
+            {
+                port.Close();
+            }
+            catch (Exception ex) { }
+         }
+
+        private void perfChart1_Load(object sender, EventArgs e) //perfChar is a online resourse used to creat a realtime chart, and I use it draw the realtime graph for accelerometer sensor signal.
+        {
+
+        }
+
+
+
+    }
+}
